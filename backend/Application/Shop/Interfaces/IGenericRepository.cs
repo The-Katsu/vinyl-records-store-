82832240c3ed6d207940807a344a8e707544aa9d
unknown <karmaelin8@gmail.com>
2022-04-29 14:00:38 +0300

    generic interface&repository

---
+++ backend/Application/Shop/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Shop.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
+    Task DeleteAsync(Guid id);
+}
---
+++ backend/Application/Shop/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Shop.Data;
+
+namespace Application.Shop.Repositories;
+
+public class GenericRepository<T> : Application.Shop.Interfaces.IGenericRepository<T> where T : class
+{
+    protected readonly ShopDbContext _context;
+    protected DbSet<T> _dbSet;
+
+    public GenericRepository(ShopDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+        => await _dbSet.ToListAsync();
+
+    public async Task<T> GetByIdAsync(Guid id)
+        => await _dbSet.FindAsync(id);
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await Commit();
+    }
+
+    public virtual async Task DeleteAsync(Guid id)
+    {
+        var e = await GetByIdAsync(id);
+        _dbSet.Remove(e);
+        await Commit();
+    }
+
+    private async Task Commit() 
+        => await _context.SaveChangesAsync();
+}
